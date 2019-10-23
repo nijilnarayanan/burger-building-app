68f2c90e00b6044c9ce30671c0e60750d8efbfb5
Nijil Narayanan <nijilnarayanan@Nijils-MacBook-Pro.local>
2019-10-23 10:08:58 +0530

    responsive sidedrawer

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -3,7 +3,7 @@ import classes from './Logo.css'
 import burgerLogo from '../../Assests/Images/logo.png'
 
 const Logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height:props.height}}>
         <img src={burgerLogo} alt='logo'/>
     </div>
 );
